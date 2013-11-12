7ffc6b385cd94b524ed6833258946dae8dc189a1
Jan Mentzel <jan@islandofwaiting.de>
2013-11-12 19:19:39 +0100

    v2 Scheduler test off

--- test/remote.php
+++ test/remote.php
@@ -4,6 +4,11 @@ require_once 'test_helper.php';
 class AllTests extends TestSuite {
 	function __construct() {
 		$this->TestSuite('Remote tests');
-		$this->collect(__DIR__.'/integration', new SimpleCollector());
+
+		// TODO: maik, v2 Scheduler not usable atm.
+		// $this->collect(__DIR__.'/integration', new SimpleCollector());
+		// TODO: maik, remove the next 2 lines when v2 Scheduler was fixed.
+		$this->addFile(__DIR__.'/integration/PaymentIntegrationTest.php');
+		$this->addFile(__DIR__.'/integration/TransactionIntegrationTest.php');
 	}
 }
