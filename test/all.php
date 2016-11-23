c643b5093156caf4f5de24da7deb025f47ca8b40
Stefan Dvoretskii <stefan.dvorezky@gmail.com>
2016-11-23 16:56:08 +0100

    Removed deprecated constructors & formatted test assumptions

--- test/all.php
+++ test/all.php
@@ -2,8 +2,8 @@
 require_once 'test_helper.php';
 
 class AllTests extends TestSuite {
-	function __construct($label=false) {
-		$this->label = $label;
+	function __construct() {
+		$this->label = 'all tests';
 		$this->collect(__DIR__.'/unit', new SimpleCollector());
 	}
 }
